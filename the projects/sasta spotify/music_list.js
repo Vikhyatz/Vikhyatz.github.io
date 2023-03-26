7d45e72598c2d8e075c9da8a71d4873ae3299c94
vikhyat gupta <108997136+Vikhyatz@users.noreply.github.com>
2023-03-26 15:34:31 +0530

    Add files via upload

---
+++ the projects/sasta spotify/music_list.js
@@ -0,0 +1,51 @@
+const allMusic = [
+    {
+        name: "It's Not So Sad",
+        artist: "dybbukk",
+        img: "image-1",
+        src: "music-1",
+        len: "2:20"
+    },
+    {
+        name: "I Got Summer On My Mind",
+        artist: "tazzy",
+        img: "image-2",
+        src: "music-2",
+        len: "3:57"
+    },
+    {
+        name: "Je M'appelle",
+        artist: "benzz",
+        img: "image-3",
+        src: "music-3",
+        len: "2:23"
+    },
+    {
+        name: "By The Sword",
+        artist: "Jake Hill",
+        img: "image-4",
+        src: "music-4",
+        len: "2:05"
+    },
+    {
+        name: "Snake",
+        artist: "Nathan Evans",
+        img: "image-5",
+        src: "music-5",
+        len: "3:44"
+    },
+    {
+        name: "faded",
+        artist: "Alan walker",
+        img: "image-6",
+        src: "music-6",
+        len: "3:32"
+    },
+    {
+        name: "Elevated",
+        artist: "Shubh",
+        img: "image-7",
+        src: "music-7",
+        len: "3:30"
+    },
+];
